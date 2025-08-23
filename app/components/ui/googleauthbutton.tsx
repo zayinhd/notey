73be21c917e8d06ee39a5e5c9cdacc876cfa8890
zayin <zayinm300@gmail.com>
2025-08-23 19:17:16 +0000

    feat(auth): separate google authbutton fromauthform ui

---
+++ app/components/ui/googleauthbutton.tsx
@@ -0,0 +1,14 @@
+import { signInWithGoogle } from "@/app/auth/actions";
+
+export default function GoogleAuthButton() {
+    return (
+        <div className="mb-5 text-center">
+            <button
+                onClick={signInWithGoogle}
+                className="w-42 m-2 p-2 bg-white text-black rounded-4xl"
+            >
+                Signin with Google
+            </button>
+        </div>
+    );
+}
