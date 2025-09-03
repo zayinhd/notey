33a641c1a382922f0fe031ca1135fb4ee6980d16
zayin <zayinm300@gmail.com>
2025-09-03 23:33:38 +0000

    added user sidebar

--- app/components/ui/navigation/navbarClient.tsx
+++ app/components/ui/navigation/navbarClient.tsx
@@ -9,39 +9,35 @@ export default function NavbarClient({ user }: { user: any }) {
 
     return (
         <>
-            {user ? (
-                <nav className="fixed top-0 left-0 right-0 h-16 bg-white shadow-md flex items-center justify-between px-6 z-50">
-                    <h1 className="text-xl font-bold">Notey</h1>
-                    <div className="">
+            <nav className="fixed top-0 left-0 right-0 h-16 bg-white shadow-md flex items-center justify-between px-6 z-50">
+                <h1 className="text-xl font-bold">Notey</h1>
+
+                <div>
+                    {user?.user_metadata?.avatar_url ? (
                         <button
-                            onClick={() => setOpen(true)}
-                            className="md:hidden p-2 rounded-lg hover:bg-gray-100"
+                            onClick={() => setOpen(!open)}
+                            className=" p-2 rounded-full hover:bg-secondary"
                         >
                             <Image
                                 src={user.user_metadata.avatar_url}
                                 alt="User profile"
                                 width={32}
                                 height={32}
-                                className="rounded-full"
+                                className="rounded-full "
                             />
                         </button>
-                    </div>
-                </nav>
-            ) : (
-                <nav className="fixed top-0 left-0 right-0 h-16 bg-white shadow-md flex items-center justify-between px-6 z-50">
-                    <h1 className="text-xl font-bold">Notey</h1>
-                    <div className="">
+                    ) : (
                         <button
                             onClick={() => setOpen(true)}
                             className="md:hidden p-2 rounded-lg hover:bg-gray-100"
                         >
                             Login
                         </button>
-                    </div>
-                </nav>
-            )}
+                    )}
+                </div>
+            </nav>
 
-            <Sidebar open={open} setOpen={setOpen} />
+            {open && <Sidebar open={open} setOpen={setOpen} />}
         </>
     );
 }
